5d52435e7b76863e14fcb673d9bc6cb65cbd1a22
l <dannynr28@gmail.com>
2020-06-17 17:52:53 -0400

    get ip, get ports

--- server/helpers/localNetworkHelper.py
+++ server/helpers/localNetworkHelper.py
@@ -1,18 +1,52 @@
 from subprocess import Popen
 from subprocess import PIPE
 
+
 class LocalNetworkHelper:
     @staticmethod
     def getIpsFromLocalNetwork():
         proc = Popen('sudo arp-scan --interface=eth0 --localnet', shell=True, stdout=PIPE, )
-        output = str(proc.communicate()[0])
-        output = output.replace("\\t", "\t").replace("\\n", "\n")
-        list_output = output.split('\n')
+        scan_str = str(proc.communicate()[0])
+        scan_str = scan_str.replace("\\t", "\t").replace("\\n", "\n")
+        list_scan = scan_str.split('\n')
 
         ips = []
-        for ip in list_output:
+        for ip in list_scan:
             ips.append(ip)
-
+        
         del ips[0:2]
-        return ips
+        del ips[-4:]
 
+        i=0        
+        for element in ips:
+            ips[i] = element.split('\t')
+            i=i+1
+        
+        key_list = []
+        for i in range(len(ips)):
+            
+            key_list.append(i)
+         
+        ip_dict = {}
+        
+        for key in key_list:
+            for ip_elements in ips:
+                ip_dict[key] = ip_elements
+
+        
+        return ip_dict
+
+    
+    def getPortsOpen():
+
+        ips = LocalNetworkHelper().getIpsFromLocalNetwork()
+        ports = []
+        for i in range(len(ips)):
+            ip = ips[i][0]
+            proc = Popen('sudo nmap -O '+ip, shell=True, stdout=PIPE, )
+            scan_ports = str(proc.communicate()[0])
+            #scan_ports = scan_ports.replace("\\n", "\n")
+            list_scan = scan_ports.split('\\n')
+            ports.append(list_scan)
+
+        return ports
